62f642c47a4007a035b989e1972ac4edbfd0fd02
Nathan Stocks <cleancut@github.com>
2018-07-14 19:57:24 -0600

    Readme, and challenge the zeroth.

---
+++ 00/main.rs
@@ -0,0 +1,13 @@
+//! Well, you found me.  I guess we better get started.
+//!
+//! CHALLENGE NUMBER ZERO!
+//! This crate isn't even a crate.  "WHAT!", you say? That is correct. A main.rs
+//! alone does not a crate make. Soooo, you'll have to fix it.
+//!
+//! Find a "cargo" command that you can run from 00/ that will add the necessary missing file(s) 
+//! to the **existing directory** so that when you do "cargo run", the message below gets printed
+//! to your console.  Run that command!  Run it, I say! FIX THE CRATE!!!
+
+fn main() {
+    println!("It feels swell to have a config file.");
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# rusty_challenge
+# Rusty Challenge!
+
 So, you want to learn Rust, eh?  See if you can survive these challenges!  Mwuhahahaha!
+
+# Instructions
+
+1. Clone this repository
+2. For each directory (Starting with `00` and then `01` and then `02`...see how this works?)
+  1. Open `src/main.rs` and read the challenge at the top of the file.
+  2. Survive the challenge.  Nay, CONQUER THE CHALLENGE!
+  3. Feel your power increasing.
+3. Having passed all the challenges, glory in the power of Rust that you
+   have obtained, and go forth and conquer the world!
